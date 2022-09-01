be93468b2866ca5013964d3c457a6cc220f4913a
Sarah Hey <49681619+y-ding-y@users.noreply.github.com>
2022-09-01 08:56:31 +0800

    Create HeaderFullscreen.vue

---
+++ src/layouts/header/HeaderFullscreen.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @click="$emit('fullscreen')" class="fullscreen-btn">
+    <a-tooltip placement="left" :title="$t('fullscreen')">
+      <a-icon type="fullscreen" />
+    </a-tooltip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderFullscreen",
+  i18n: {
+    messages: {
+      cn: {
+        fullscreen: "全屏显示",
+      },
+      tw: {
+        fullscreen: "全屏顯示",
+      },
+      us: {
+        fullscreen: "full screen display",
+      },
+    },
+  },
+
+  
+};
+</script>
+
+<style lang="less" scoped>
+.fullscreen-btn {
+  margin-right: 2px;
+  font-size: 100%;
+  &:hover {
+    margin-right: 0;
+    font-size: 150%;
+    color: @primary-color;
+  }
+}
+</style>
