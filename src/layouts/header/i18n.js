84b90d6a45ea8e392d408ac2ad1f63b9bb61e7dd
Sarah Hey <49681619+y-ding-y@users.noreply.github.com>
2022-09-01 08:58:11 +0800

    Create i18n.js

---
+++ src/layouts/header/i18n.js
@@ -0,0 +1,31 @@
+module.exports = {
+  messages: {
+    cn: {
+      usercenter: '个人中心',
+      userfavor: '我的最爱',
+      usersetting: '设定',
+      userlogout: '退出登录',
+      favordelete: '已经删除 {name}',
+      favorinsert: '已经收藏 {name}',
+      bye: '再见!',
+    },
+    tw: {
+      usercenter: '個人中心',
+      userfavor: '我的最愛',
+      usersetting: '設定',
+      userlogout: '退出登錄',
+      favordelete: '已經刪除 {name}',
+      favorinsert: '已經收藏 {name}',
+      bye: '再見!',
+    },
+    us: {
+      usercenter: 'User center',
+      userfavor: 'Favorites',
+      usersetting: 'Setting',
+      userlogout: 'Logout',
+      favordelete: 'delete {name} from favors',
+      favorinsert: 'add {name} into favors',
+      bye: 'Bye bye!',
+    }
+  }
+}
